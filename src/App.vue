743632577bd134e8957a6e2512d0a554ff726aa4
Jero1304 <calogeroleto13@gmail.com>
2023-03-03 16:43:34 +0100

    FEAT header

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+</script>
+
+<template>
+  <AppMain/>
+  <AppHeader/>
+</template>
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="main-header">
+        <div class="container">
+            <img :src="logo" alt="">
+            <h1>{{ title }}</h1>   
+        </div>
+    </header>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            title:'Yu-Gi-Oh API',
+            logo:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg'
+        }
+    }
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+.main-header{
+    text-align: center;
+    font-size: 18px;
+    padding: 20px;
+    .container{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: start;
+
+        img{
+            max-width: 70px;
+        }
+        h1{
+            padding-left: 15px;
+        }
+    }
+
+}
+</style>
